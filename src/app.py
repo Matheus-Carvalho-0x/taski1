a9428b939e63e09d3113c138cff406925b6052d5
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-03-24 04:07:14 -0300

    Added sqlite3 as databank, also no more lists being used to store data, no more flag to control when the code stops.

--- src/app.py
+++ src/app.py
@@ -1,9 +1,6 @@
-flag = True
-item_list = []
-i = 0
-x = True
+import db
 
-while flag:
+while 1:
     print("#======================================#")
     choice = int(input("""Type: 
 1 - Add a new item;
@@ -15,32 +12,25 @@ while flag:
     match choice:
         case 1:
             item = input("Type a new item: ")
-            i += 1
-            item_list.append([i, item])
+            db.startDB((1, item))
             print("New item added!")
         case 2:
+            task_list = db.startDB((2,))
             print("Item list: ")
-            for item in item_list:
+            for item in task_list:
                 print(f"{item[0]} - {item[1]}")
         case 3:
             change = int(input("Choose an item to change: "))
             new_data = input("Type the new item: ")
-            for item in item_list:
-                if item[0] == change:
-                    item[1] = new_data
-                    print("Changed!")
-                    x = False
-            if x:
-                print("Item not found!")
+            db.startDB((3, (change, new_data)))
+            print("Changed!")
         case 4:
             delete = int(input("Choose an item to delete: "))
-            for item in item_list:
-                if item[0] == delete:
-                    item_list.pop(delete-1)
+            db.startDB((4, delete))
             print("Item deleted!")
         case 5:
             print("Bye!")
-            flag = False
+            break
         case _:
             print("Invalid Value!")
     print("#======================================#")
---
+++ src/db.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import sqlite3
+
+def startDB(data):
+    # Start
+    data = data
+
+    # Extra safety to create the .db file
+    BASE_DIR = Path(__file__).resolve().parent
+    db_path = BASE_DIR.parent/"db"/"databank.db"
+
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                task TEXT NOT NULL
+                )""")
+    
+    # Functions: create, read, update, delete
+    def insert(data):
+        cursor.execute(f"""INSERT INTO tasks (task)
+                       VALUES ('{data}')""")
+
+    def select():
+        cursor.execute(f"""SELECT * FROM tasks""")
+        task_list = cursor.fetchall()
+        return task_list
+
+    def update(data):
+        cursor.execute(f"""UPDATE tasks SET task = '{data[1]}' 
+                       WHERE id = {data[0]}""")
+
+    def delete(data):
+        cursor.execute(f"DELETE FROM tasks WHERE id = {data}")
+
+
+    # Operational
+    match data[0]:
+        case 1:
+            insert(data[1])
+        case 2:
+            tasks = select()
+            return tasks
+        case 3:
+            update(data[1])
+        case 4:
+            delete(data[1])
+
+    connection.commit()
